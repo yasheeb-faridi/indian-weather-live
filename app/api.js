6bef70935c1c9363c926371a6e5c8a895e5c0cb8
Yasheeb Faridi <yasheebfaridi@proton.me>
2025-12-03 18:50:36 +0530

    Created weather.jsx and added the .env file

--- app/api.js
+++ app/api.js
@@ -1,8 +1,8 @@
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/places/%7BplaceId%7D/distance?toPlaceId=Q60';
-export const geoAPIcityOptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '2fb3157b60mshfee705214ea2538p1aea86jsnc16edd478938',
-		'x-rapidapi-host': 'geodb-cities.p.rapidapi.com'
-	}
-};
+// export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/places/%7BplaceId%7D/distance?toPlaceId=Q60';
+// export const geoAPIcityOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '2fb3157b60mshfee705214ea2538p1aea86jsnc16edd478938',
+// 		'x-rapidapi-host': 'geodb-cities.p.rapidapi.com'
+// 	}
+// };
--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import Image from "next/image";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 import axios from "axios";
 import { useState } from "react";
-import {BsSearch} from 'react-icons/bs'
-
+import { BsSearch } from "react-icons/bs";
+import Weather from "./components/Weather";
 
 export default function Home() {
-  const [city, setCity] = useState('')
-  const [weather, setWeather] = useState({})
-  const [loading, setLoading] = useState(false)    // Have to find out?
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false); // Have to find out?
 
-  // const url = ''                                /// to put the weather API url 
+  // const url = ''                                /// to put the weather API url
 
   // const fetchWeather = (e) => {
   //   e.preventDefault()       // this will prevent the page to reload
@@ -30,21 +30,25 @@ export default function Home() {
       {/* <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <Search/>
       </main> */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 z-1 bg-black/50" />
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-1" />
       {/* Background Image */}
-      <Image src="/golden-autumn.avif" layout="fill" alt="bg-wallpaper" />
+      <Image src="/sunrise-farm.jpg" layout="fill" alt="bg-wallpaper" />
       {/* <Search className="relative flex justify-between max-w-500px" /> */}
 
       {/* Search City */}
-      <div className="relative flex justify-between items-center max-w-[500px] w-full margin-auto pt-4 ">
-        <form>
-          <div>
-            <input type="text" placeholder="Type City to Search"></input>
-          </div>
-          <button><BsSearch/></button>
+      <div className="relative flex justify-between items-center max-w-[500px] w-full margin-auto pt-4 z-10">
+        <form className="flex justify-between items-center w-full m-auto p-4 bg-transparent border border-gray-500 rounded-3xl ">
+          <input type="text" placeholder="Type City to Search" className="bg-transparent border-none focus:outline-none text-2xl"></input>
+
+          <button>
+            <BsSearch size={20} />
+          </button>
         </form>
       </div>
       {/* <button onClick={fetchWeather}>Refresh</button> */}
+
+      {/* Weather */}
+      {weather.main && <Weather/>}
     </div>
   );
 }
